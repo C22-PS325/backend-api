c23a775fb124a8ff0688079442d22e2eb8787efb
nazhiifyahya <mnazhiifys@gmail.com>
2022-05-28 14:53:30 +0700

    fix register, login, logout

--- src/handler.js
+++ src/handler.js
@@ -1,12 +1,12 @@
 const { startConnection } = require('./db-conn');
 
-const registerPatient = (request, h) => {
+const registerPatient = async (request, h) => {
   const { username, password } = request.payload;
   const query = `INSERT INTO user_account (username, password, role) VALUES ("${username}", "${password}", "patient")`;
 
-  connection.query(query, (err) => {
-    if (err) throw err;
-  });
+  const conn = await startConnection();
+  await conn.query(query);
+  await conn.end();
 
   const response = h.response({
     status: 'success',
@@ -24,15 +24,16 @@ const loginPatient = async (request, h) => {
   const { username, password } = request.payload;
 
   const conn = await startConnection();
-
   const result = await conn.query('SELECT password FROM user_account WHERE username = ?', [username]);
+  conn.end();
 
   const querriedPassword = result[0][0].password;
-
   if (password === querriedPassword) {
     const token = '123456';
-    // console.log(token);
-    await conn.query('UPDATE user_account SET token = "?" WHERE username = "?"', [token, username]);
+
+    const conn2 = await startConnection();
+    await conn2.query('UPDATE user_account SET token = ? WHERE username = ?', [token, username]);
+    conn2.end();
 
     const response = h.response({
       status: 'success',
@@ -55,13 +56,16 @@ const loginPatient = async (request, h) => {
 
 const logoutPatient = async (request, h) => {
   const { username, token } = request.payload;
-  console.log(token);
+
   const conn = await startConnection();
-  const result = await conn.query('SELECT token FROM user_account WHERE username = "?"', [username]);
+  const result = await conn.query('SELECT token FROM user_account WHERE username = ?', [username]);
+  await conn.end();
 
   const querriedToken = result[0][0].token;
   if (token === querriedToken) {
-    await conn.query('UPDATE user_account SET token = "" WHERE username = "?"', [username]);
+    const conn2 = await startConnection();
+    await conn2.query('UPDATE user_account SET token = "" WHERE username = ?', [username]);
+    conn2.end();
 
     const response = h.response({
       status: 'success',
